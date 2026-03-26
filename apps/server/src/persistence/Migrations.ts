tmacc/agentdynamo

Persist provider-aware model selections (#1371)

apps/server/src/persistence/Migrations.ts
@@ -10,6 +10,7 @@
 
 import * as Migrator from "effect/unstable/sql/Migrator";
 import * as Layer from "effect/Layer";
+import * as Effect from "effect/Effect";
 
 // Import all migrations statically
 import Migration0001 from "./Migrations/001_OrchestrationEvents.ts";
@@ -27,7 +28,7 @@ import Migration0012 from "./Migrations/012_ProjectionThreadsInteractionMode.ts"
 import Migration0013 from "./Migrations/013_ProjectionThreadProposedPlans.ts";
 import Migration0014 from "./Migrations/014_ProjectionThreadProposedPlanImplementation.ts";
 import Migration0015 from "./Migrations/015_ProjectionTurnsSourceProposedPlan.ts";
-import { Effect } from "effect";
+import Migration0016 from "./Migrations/016_CanonicalizeModelSelections.ts";
 
 /**
  * Migration loader with all migrations defined inline.
@@ -39,29 +40,43 @@ import { Effect } from "effect";
  * Uses Migrator.fromRecord which parses the key format and
  * returns migrations sorted by ID.
  */
-const loader = Migrator.fromRecord({
-  "1_OrchestrationEvents": Migration0001,
-  "2_OrchestrationCommandReceipts": Migration0002,
-  "3_CheckpointDiffBlobs": Migration0003,
-  "4_ProviderSessionRuntime": Migration0004,
-  "5_Projections": Migration0005,
-  "6_ProjectionThreadSessionRuntimeModeColumns": Migration0006,
-  "7_ProjectionThreadMessageAttachments": Migration0007,
-  "8_ProjectionThreadActivitySequence": Migration0008,
-  "9_ProviderSessionRuntimeMode": Migration0009,
-  "10_ProjectionThreadsRuntimeMode": Migration0010,
-  "11_OrchestrationThreadCreatedRuntimeMode": Migration0011,
-  "12_ProjectionThreadsInteractionMode": Migration0012,
-  "13_ProjectionThreadProposedPlans": Migration0013,
-  "14_ProjectionThreadProposedPlanImplementation": Migration0014,
-  "15_ProjectionTurnsSourceProposedPlan": Migration0015,
-});
+export const migrationEntries = [
+  [1, "OrchestrationEvents", Migration0001],
+  [2, "OrchestrationCommandReceipts", Migration0002],
+  [3, "CheckpointDiffBlobs", Migration0003],
+  [4, "ProviderSessionRuntime", Migration0004],
+  [5, "Projections", Migration0005],
+  [6, "ProjectionThreadSessionRuntimeModeColumns", Migration0006],
+  [7, "ProjectionThreadMessageAttachments", Migration0007],
+  [8, "ProjectionThreadActivitySequence", Migration0008],
+  [9, "ProviderSessionRuntimeMode", Migration0009],
+  [10, "ProjectionThreadsRuntimeMode", Migration0010],
+  [11, "OrchestrationThreadCreatedRuntimeMode", Migration0011],
+  [12, "ProjectionThreadsInteractionMode", Migration0012],
+  [13, "ProjectionThreadProposedPlans", Migration0013],
+  [14, "ProjectionThreadProposedPlanImplementation", Migration0014],
+  [15, "ProjectionTurnsSourceProposedPlan", Migration0015],
+  [16, "CanonicalizeModelSelections", Migration0016],
+] as const;
+
+export const makeMigrationLoader = (throughId?: number) =>
+  Migrator.fromRecord(
+    Object.fromEntries(
+      migrationEntries
+        .filter(([id]) => throughId === undefined || id <= throughId)
+        .map(([id, name, migration]) => [`${id}_${name}`, migration]),
+    ),
+  );
 
 /**
  * Migrator run function - no schema dumping needed
  * Uses the base Migrator.make without platform dependencies
  */
 const run = Migrator.make({});
+
+export interface RunMigrationsOptions {
+  readonly toMigrationInclusive?: number | undefined;
+}
 
 /**
  * Run all pending migrations.
@@ -73,11 +88,19 @@ const run = Migrator.make({});
  *
  * @returns Effect containing array of executed migrations
  */
-export const runMigrations = Effect.gen(function* () {
-  yield* Effect.log("Running migrations...");
-  yield* run({ loader });
-  yield* Effect.log("Migrations ran successfully");
-});
+export const runMigrations = ({ toMigrationInclusive }: RunMigrationsOptions = {}) =>
+  Effect.gen(function* () {
+    yield* Effect.log(
+      toMigrationInclusive === undefined
+        ? "Running all migrations..."
+        : `Running migrations 1 through ${toMigrationInclusive}...`,
+    );
+    const executedMigrations = yield* run({ loader: makeMigrationLoader(toMigrationInclusive) });
+    yield* Effect.log("Migrations ran successfully").pipe(
+      Effect.annotateLogs({ migrations: executedMigrations.map(([id, name]) => `${id}_${name}`) }),
+    );
+    return executedMigrations;
+  });
 
 /**
  * Layer that runs migrations when the layer is built.
@@ -96,4 +119,4 @@ export const runMigrations = Effect.gen(function* () {
  * )
  * ```
  */
-export const MigrationsLive = Layer.effectDiscard(runMigrations);
+export const MigrationsLive = Layer.effectDiscard(runMigrations());

ashvinnihalani/t3code

apps/server/src/persistence/Migrations.ts
@@ -10,6 +10,7 @@
 
 import * as Migrator from "effect/unstable/sql/Migrator";
 import * as Layer from "effect/Layer";
+import * as Effect from "effect/Effect";
 
 // Import all migrations statically
 import Migration0001 from "./Migrations/001_OrchestrationEvents.ts";
@@ -29,7 +30,7 @@ import Migration0014 from "./Migrations/014_ProjectionProjectsRemote.ts";
 import Migration0015 from "./Migrations/015_ProjectionTurnsCheckpointDiff.ts";
 import Migration0016 from "./Migrations/016_ProjectionThreadProposedPlanImplementation.ts";
 import Migration0017 from "./Migrations/017_ProjectionTurnsSourceProposedPlan.ts";
-import { Effect } from "effect";
+import Migration0018 from "./Migrations/018_CanonicalizeModelSelections.ts";
 
 /**
  * Migration loader with all migrations defined inline.
@@ -41,31 +42,45 @@ import { Effect } from "effect";
  * Uses Migrator.fromRecord which parses the key format and
  * returns migrations sorted by ID.
  */
-const loader = Migrator.fromRecord({
-  "1_OrchestrationEvents": Migration0001,
-  "2_OrchestrationCommandReceipts": Migration0002,
-  "3_CheckpointDiffBlobs": Migration0003,
-  "4_ProviderSessionRuntime": Migration0004,
-  "5_Projections": Migration0005,
-  "6_ProjectionThreadSessionRuntimeModeColumns": Migration0006,
-  "7_ProjectionThreadMessageAttachments": Migration0007,
-  "8_ProjectionThreadActivitySequence": Migration0008,
-  "9_ProviderSessionRuntimeMode": Migration0009,
-  "10_ProjectionThreadsRuntimeMode": Migration0010,
-  "11_OrchestrationThreadCreatedRuntimeMode": Migration0011,
-  "12_ProjectionThreadsInteractionMode": Migration0012,
-  "13_ProjectionThreadProposedPlans": Migration0013,
-  "14_ProjectionProjectsRemote": Migration0014,
-  "15_ProjectionTurnsCheckpointDiff": Migration0015,
-  "16_ProjectionThreadProposedPlanImplementation": Migration0016,
-  "17_ProjectionTurnsSourceProposedPlan": Migration0017,
-});
+export const migrationEntries = [
+  [1, "OrchestrationEvents", Migration0001],
+  [2, "OrchestrationCommandReceipts", Migration0002],
+  [3, "CheckpointDiffBlobs", Migration0003],
+  [4, "ProviderSessionRuntime", Migration0004],
+  [5, "Projections", Migration0005],
+  [6, "ProjectionThreadSessionRuntimeModeColumns", Migration0006],
+  [7, "ProjectionThreadMessageAttachments", Migration0007],
+  [8, "ProjectionThreadActivitySequence", Migration0008],
+  [9, "ProviderSessionRuntimeMode", Migration0009],
+  [10, "ProjectionThreadsRuntimeMode", Migration0010],
+  [11, "OrchestrationThreadCreatedRuntimeMode", Migration0011],
+  [12, "ProjectionThreadsInteractionMode", Migration0012],
+  [13, "ProjectionThreadProposedPlans", Migration0013],
+  [14, "ProjectionProjectsRemote", Migration0014],
+  [15, "ProjectionTurnsCheckpointDiff", Migration0015],
+  [16, "ProjectionThreadProposedPlanImplementation", Migration0016],
+  [17, "ProjectionTurnsSourceProposedPlan", Migration0017],
+  [18, "CanonicalizeModelSelections", Migration0018],
+] as const;
+
+export const makeMigrationLoader = (throughId?: number) =>
+  Migrator.fromRecord(
+    Object.fromEntries(
+      migrationEntries
+        .filter(([id]) => throughId === undefined || id <= throughId)
+        .map(([id, name, migration]) => [`${id}_${name}`, migration]),
+    ),
+  );
 
 /**
  * Migrator run function - no schema dumping needed
  * Uses the base Migrator.make without platform dependencies
  */
 const run = Migrator.make({});
+
+export interface RunMigrationsOptions {
+  readonly toMigrationInclusive?: number | undefined;
+}
 
 /**
  * Run all pending migrations.
@@ -77,11 +92,19 @@ const run = Migrator.make({});
  *
  * @returns Effect containing array of executed migrations
  */
-export const runMigrations = Effect.gen(function* () {
-  yield* Effect.log("Running migrations...");
-  yield* run({ loader });
-  yield* Effect.log("Migrations ran successfully");
-});
+export const runMigrations = ({ toMigrationInclusive }: RunMigrationsOptions = {}) =>
+  Effect.gen(function* () {
+    yield* Effect.log(
+      toMigrationInclusive === undefined
+        ? "Running all migrations..."
+        : `Running migrations 1 through ${toMigrationInclusive}...`,
+    );
+    const executedMigrations = yield* run({ loader: makeMigrationLoader(toMigrationInclusive) });
+    yield* Effect.log("Migrations ran successfully").pipe(
+      Effect.annotateLogs({ migrations: executedMigrations.map(([id, name]) => `${id}_${name}`) }),
+    );
+    return executedMigrations;
+  });
 
 /**
  * Layer that runs migrations when the layer is built.
@@ -100,4 +123,4 @@ export const runMigrations = Effect.gen(function* () {
  * )
  * ```
  */
-export const MigrationsLive = Layer.effectDiscard(runMigrations);
+export const MigrationsLive = Layer.effectDiscard(runMigrations());